590280c969ef158b211c178effde86aba7f14711
zack8361 <71763096+zack8361@users.noreply.github.com>
2022-10-19 23:55:33 +0900

    eox
    eox

---
+++ src/Array/ex05.java
@@ -0,0 +1,16 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ex05 {
+    public static void main(String[] args) {
+        Scanner kong = new Scanner(System.in);
+        int num = kong.nextInt();
+        int[] Array = new int[num+1];
+
+        for(int i=0; i<Array.length; i++){
+            int number = Integer.parseInt(kong.nextLine());
+
+        }
+    }
+}
