75724b9cf5bd704980be55b368cd02ef383959ec
zack8361 <71763096+zack8361@users.noreply.github.com>
2022-11-04 17:56:46 +0900

    SWEA 2문제(회사)
    SWEA 2문제(회사) -진짜 개못한다

---
+++ src/SSAFY02/SWEA1954.java
@@ -0,0 +1,16 @@
+package SSAFY02;
+
+import java.util.Scanner;
+
+//달팽이 , 상하 좌우 로 틀어서 배열 입력하는 알고리즘
+public class SWEA1954 {
+    public static void main(String[] args) {
+        Scanner kong = new Scanner(System.in);
+        int tc = kong.nextInt();
+
+        for(int t=0; t<tc; t++){
+            int num = kong.nextInt();
+            int[][] arr = new int[num][num];
+        }
+    }
+}
