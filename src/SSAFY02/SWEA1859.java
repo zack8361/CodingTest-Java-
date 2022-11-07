b5b997265bc02b8103f450062594083b63e487d8
zack8361 <71763096+zack8361@users.noreply.github.com>
2022-11-07 17:57:30 +0900

    2문제 집에서 풀어봐
    2문제 풀어보는게 목적

---
+++ src/SSAFY02/SWEA1859.java
@@ -0,0 +1,16 @@
+package SSAFY02;
+
+
+import java.util.Scanner;
+
+//백만 장자 프로젝트
+public class SWEA1859 {
+    public static void main(String[] args) {
+        Scanner kong = new Scanner(System.in);
+        int tc = kong.nextInt();
+        for(int t=0; t<tc; t++){
+            int N = kong.nextInt();
+            int[] arr = new int[N];
+        }
+    }
+}
---
+++ src/SSAFY02/SWEA1940.java
@@ -0,0 +1,7 @@
+package SSAFY02;
+
+public class SWEA1940 {
+    public static void main(String[] args) {
+
+    }
+}
