62c8abe120ab0dfb0ceb68634f728c0a95cf4113
zack8361 <71763096+zack8361@users.noreply.github.com>
2022-11-30 23:20:57 +0900

    집(예습) + 회사에서 문제 풀기 백준
    꼭 풀어봐

---
+++ src/알고리즘4탐색/BinEx2512.java
@@ -0,0 +1,12 @@
+package 알고리즘4탐색;
+
+import java.util.Scanner;
+
+public class BinEx2512 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+    }
+}
---
+++ src/알고리즘4탐색/BinEx2805.java
@@ -0,0 +1,39 @@
+package 알고리즘4탐색;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinEx2805 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(); //베열의 개수
+        int M = sc.nextInt(); //구해야 하는값
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int start = 0;
+        int end = arr[arr.length-1];
+        int mid =0;
+        while (start<=end){
+            mid = (start + end)/2;
+            long sum =0;
+            for(int i=0; i<N; i++){
+                if(arr[i]>mid){//여기서 arr[i]=배열의 원소
+                    sum+=arr[i]-mid;
+                }
+            }
+            if(sum>=M){
+                start = mid +1;
+            }
+            else {
+                end = mid-1;
+            }
+
+        }
+        System.out.println(end);
+    }
+}
