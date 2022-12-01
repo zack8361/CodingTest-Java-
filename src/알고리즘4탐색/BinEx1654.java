cf7a3a55f77989b376a49d7e756a483145e32ee2
zack8361 <71763096+zack8361@users.noreply.github.com>
2022-12-01 17:39:46 +0900

    백준 두문제
    백준 두문제 집가서 가능하면 복습 까지 하고자기.

---
+++ src/알고리즘4탐색/BinEx1654.java
@@ -0,0 +1,31 @@
+package 알고리즘4탐색;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinEx1654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        int start = 0;
+        int end = arr[arr.length-1];
+        int mid =0;
+        int count = 0;
+        while (start<=end){
+            mid = (start+end)/2;
+            for(int i=0; i<N; i++){
+                if(arr[i]>mid){
+
+                }
+            }
+        }
+    }
+}
--- src/알고리즘4탐색/BinEx2512.java
+++ src/알고리즘4탐색/BinEx2512.java
@@ -1,5 +1,6 @@
 package 알고리즘4탐색;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BinEx2512 {
@@ -7,6 +8,37 @@ public class BinEx2512 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[] arr = new int[N];
+        int answer = 0;
 
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int M = sc.nextInt();
+        Arrays.sort(arr);
+        int start = 0;
+        int end = arr[arr.length - 1];
+        int mid = 0;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            long sum = 0;
+            for (int i = 0; i < N; i++) {
+                if (arr[i] > mid) {
+                    //배열이 지정한 mid 보다 크다면
+                    sum += mid;
+                    //mid 를 더해라
+                } else { //arr[i] <mid
+                    sum += arr[i];
+                }
+            }
+            if (sum < M) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+                answer = Math.max(answer, mid);
+            }
+        }
+        System.out.println(end);
+        System.out.println(answer);
     }
 }
