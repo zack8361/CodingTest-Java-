be96377db477d44185ffae4dc9da8df44d911650
zack8361 <71763096+zack8361@users.noreply.github.com>
2022-12-27 16:20:31 +0900

    가장 큰수(기수정렬, sort) 방식으로 풀어보기
    가장 큰수(기수정렬, sort) 방식으로 풀어보기

---
+++ src/Programmers2/BigNumber.java
@@ -0,0 +1,21 @@
+package Programmers2;
+
+import javafx.print.Collation;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+//public class BigNumber {
+//    public static void main(String[] args) {
+//        int[] numbers = {3,30,34,5,9};
+//        System.out.println(solution(numbers));
+//    }
+
+//    private static String solution(int[] numbers) {
+//        String answer = "";
+//        for(int i=0; i<)
+//        return answer;
+//    }
+//}
+
+//집가서 풀어보기.
