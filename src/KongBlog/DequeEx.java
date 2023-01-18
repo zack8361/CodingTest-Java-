b52ef25fcc9476e9d757174d8133eb3067809633
zack8361 <71763096+zack8361@users.noreply.github.com>
2023-01-18 17:17:00 +0900

    Dequeue(기본 덱)
    Dequeue(기본 덱)

---
+++ src/KongBlog/DequeEx.java
@@ -0,0 +1,17 @@
+package KongBlog;
+
+import java.util.ArrayDeque;
+
+/**
+ * Double ended Queue
+ * 양쪽에서 데이터를 삽입 삭제를 할수 있다. -> 아주 좋다..
+ */
+
+public class DequeEx {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+
+
+    }
+}
